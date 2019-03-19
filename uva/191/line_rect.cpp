fb29b046071332538a670a71e07980c818275bef
Anshil Gandhi <gandhi21299@gmail.com>
2019-03-19 12:16:47 -0600

    modified uva/191

--- uva/191/line_rect.cpp
+++ uva/191/line_rect.cpp
@@ -1,67 +1,71 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <complex>
 using namespace std;
+typedef complex<double> point;
 
-struct point{
-	int x, y;
-	point(){}
-	point(int xx, int yy)	:	x(xx), y(yy)	{}
-};
+#define eps 1e-6
 
-bool onSegment(point p, point q, point r){
-	if (q.x <= max(p.x, r.x) and q.x >= min(p.x, r.x) and
-		q.y <= max(p.y, r.y) and q.y >= min(p.y, r.y))
-		return true;
-	return false;
+double cross(point a, point b){	return imag(conj(a)*b);	}
+double dot(point a, point b){	return real(conj(a)*b);	}
+
+int line_line(point a, point b, point c, point d, point& p){
+	if (fabs(cross(b-a, d-c)) > eps){
+		p = cross(c-a, d-c) / cross(b-a, d-c) * (b-a) + a;
+		return 1;
+	}
+	if (fabs(cross(b-a, b-c)))	return 0;
+	return -1;
 }
 
-int orientation(point p, point q, point r){
-	int val = (q.y - p.y) * (r.x - q.x) -
-				(q.x - p.x) * (r.y - q.y);
-	if (val == 0)	return 0;
-	return (val > 0)	? 1 : 2;
-}
-
-bool intersect(point p1, point q1, point p2, point q2){
-	int o1 = orientation(p1, q1, p2);
-	int o2 = orientation(p1, q1, q2);
-	int o3 = orientation(p2, q2, p1);
-	int o4 = orientation(p2, q2, q1);
-
-	if (o1 != o2 and o3 != o4)	return true;
-
-	if (o1 == 0 and onSegment(p1, p2, q1))	return true;
-	if (o2 == 0 and onSegment(p1, q2, q1))	return true;
-	if (o3 == 0	and onSegment(p2, p1, q2))	return true;
-	if (o4 == 0 and onSegment(p2, q1, q2))	return true;
-	return false;
+int seg_seg(point a, point b, point c, point d, point& p){
+	int s = line_line(a, b, c, d, p);
+	if (s == 0)	return 0;
+	if (s == -1){
+		if (dot(a-c, a-d) < eps){
+			return 2;
+		}
+		if (dot(b-c, b-d) < eps){
+			return 2;
+		}
+		if (dot(c-a, c-b) < eps){
+			return 2;
+		}
+		return -1;
+	}
+	if (dot(p-a, p-b) < eps and dot(p-c, p-d) < eps)	return 1;
+	return -2;
 }
 
 int main(){
 	int n;
 	cin >> n;
 
-	for (int i =0 ; i < n; ++i){
-		point p1, q1, a, b;
-		cin>>p1.x >> p1.y >> q1.x >> q1.y >> a.x >> a.y >> b.x >> b.y;
-		
-		// line and top edge
-		if (intersect(p1, q1, a, point(b.x, a.y))){
+	while (n--){
+		int xstart,ystart, xend, yend, xleft, ytop, xright, ybottom;
+		cin >> xstart >> ystart >> xend >> yend >> xleft >> ytop;
+		cin >> xright, ybottom;
+
+		point p(xstart, ystart);
+		point q(xend, yend);
+		point a(xleft, ytop);
+		point b(xleft, ybottom);
+		point c(xright, ytop);
+		point d(xright, ybottom);
+
+		point tmp;
+
+		if (seg_seg(p, q, a, b, tmp) or seg_seg(p, q, a, c, tmp)){
 			cout << "T" << endl;
+			continue;
 		}
-		else if (intersect(p1, q1, a, point(a.x, b.y))){
+		if (seg_seg(p, q, b, d, tmp) or seg_seg(p, q, c, d, tmp)){
 			cout << "T" << endl;
+			continue;
 		}
-		else if (intersect(p1, q1, b, point(a.x, b.y))){
-			cout << "T" << endl;
-		}
-		else if (intersect(p1, q1, b, point(b.x, a.y))){
-			cout << "T" << endl;
-		}
-		else{
-			cout << "F" << endl;
-		}
+		cout << "F" << endl;
 
 	}
 
 	return 0;
 }
+
