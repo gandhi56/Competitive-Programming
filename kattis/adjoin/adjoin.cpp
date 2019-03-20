673661df38111b50977b3623b5f1aa8b234962cf
Anshil Gandhi <gandhi21299@gmail.com>
2019-03-19 18:17:16 -0600

    solve adjoin the networks

--- kattis/adjoin/adjoin.cpp
+++ kattis/adjoin/adjoin.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#include <algorithm>
+#include <cstring>
 #define INF 10000007
 
 using namespace std;
@@ -9,9 +10,6 @@ typedef vector< vector<int> > graph;
 int c, l;
 
 void bfs(graph& g, int s, int* dist){
-	for (int i = 0; i < g.size(); ++i){
-		dist[i] = -1;
-	}
 	queue<int> q;
 	q.push(s);
 	dist[s] = 0;
@@ -28,18 +26,38 @@ void bfs(graph& g, int s, int* dist){
 
 int main(){
 	cin >> c >> l;
-
 	graph g(c);
-
 	for (int i =0 ; i < l; ++i){
 		int u, v;
 		cin >> u >> v;
-
 		g[u].push_back(v);
 		g[v].push_back(u);
 	}
 
 	// find the node with the largest diameter
+	int k1;
+	int dist[c], id[c];
+	memset(dist, -1, sizeof(dist));
+	for (int i = 0; i < g.size(); ++i){
+		if (dist[i] != -1)	continue;
+		bfs(g, i, dist);
+	}
+
+	k1 = *max_element(dist, dist+c);
+	memset(dist, -1, sizeof(dist));
+
+	bfs(g, k1, dist);
+
+	for (int i = 0; i < c; ++i){
+		if (dist[i] != -1)	id[i] = 1;
+	}
+	int currId = 2;
+	for (int i = 0; i < c; ++i){
+		if (dist[i] == -1){
+			bfs(g, i, dist);
+			for ()
+		}
+	}
 
 	return 0;
 }
