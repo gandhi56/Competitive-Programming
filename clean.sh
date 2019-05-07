49c62296b17ed0327d8ea990686f317e4aabacb9
gandhi56 <gandhi21299@gmail.com>
2019-05-06 23:18:16 -0600

    cleaned up files

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+find . -name "*.out" -type f
+find . -name "*.ans" -type f
+find . -name "*.in" -type f
+find . -name "*.sw[klmnop]" -type f
+echo Should I delete these files?[y/n]
+
+
+read user
+if [ "$user" == "y" ]; then
+	find . -name "*.out" -type f -delete
+	find . -name "*.ans" -type f -delete
+	find . -name "*.in" -type f -delete
+	find . -name "*.sw[klmnop]" -type f -delete
+	echo Deleted!
+fi
