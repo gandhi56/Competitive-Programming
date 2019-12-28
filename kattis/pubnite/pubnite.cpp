080cebbd3d2f93d89a24126fc9965a4ca0320631
gandhi56 <gandhi21299@gmail.com>
2019-12-27 23:39:32 -0700

    finally solved pubnite

--- kattis/pubnite/pubnite.cpp
+++ kattis/pubnite/pubnite.cpp
@@ -1,33 +1,87 @@
 #include <bits/stdc++.h>
 #define EPS 1e-6
-
 using namespace std;
-long double sq(long double x){
+
+typedef long double ld;
+
+ld sq(ld x){
 	return x*x;
 }
 
+ld xs, ys, ss, ri, rf;
+ld xa, ya, sa;
+
+ld rad(){
+	return sqrt( sq(xa - xs) + sq(ya - ys) );
+}
+
+bool is_safe(ld x, ld y, ld r){
+	return (sq(x - xs) + sq(y - ys)) <= sq(r);
+}
+
 int main(){
-	long double xs, ys, ss, ri, rf;
 	cin >> xs >> ys >> ss >> ri >> rf;
-
-	long double xa, ya, sa;
 	cin >> xa >> ya >> sa;
-
-	long double theta = (long double)atan(fabs(ya-ys)/fabs(xa-xs));
-	long double dx = sa * (long double)cos(theta);
-	long double dy = sa * (long double)sin(theta);
-
-	long double d = 0.0;
-	while (rf + EPS < ri){
-		if (sq(ri) + EPS < sq(xa - xs) + sq(ya - ys))	d += 1.0;
-		xa += dx;
-		ya += dy;
-		ri -= ss;
-	}
 
 	cout.setf(ios::fixed);
 	cout.precision(8);
-	cout << d << endl;
+
+	if (is_safe(xa, ya, ri)){
+		if (ss > sa){
+			// Anthony is in circle initally, but runs slower than the radius
+			// Anthony can get out of the circle after t sec.
+			// circle keeps shrinking, Anthony keeps running;
+			// circle stops shrinking after rt sec. --> Anthony is unsafe
+			// Anthony keeps running until he reaches rf
+			// 0 - t : safe
+			// t - rt : unsafe
+			// time until Anthony reaches rf
+			//cout << "1" << endl;
+			ld ai = rad();
+			ld t = (ri - ai) / (ss - sa);	// time when circle surpasses anthony
+			ld rt = (ri - rf) / ss;	// time until circle stops shrinking
+			ld ap = ai - sa*rt;	// Anthony's position after rt sec.
+			ld at = 0.0;
+			if (ap > rf){
+				at = (ap - rf) / sa;
+				cout << rt - t + at << endl;
+			}
+			else{
+				cout << 0.0 << endl;
+			}
+    }
+		else{
+			// No damage, Anthony is always safe
+			//cout << "2" << endl;
+			cout << 0.0 << endl;
+		}
+	}
+	else{
+    // Anthony is outside of the safe zone
+		if (ss >= sa){
+			// this is good now!
+			ld rt = (ri - rf) / ss;
+			ld ai = rad();
+			ld ap = ai - sa*rt;	// Anthony's position after rt sec.
+			ld at = (ap - rf) / sa;
+			cout << rt + at << endl;
+		}
+		else{
+			// Anthony faster than the circle
+			ld ai = rad();
+			ld t = (ri - ai) / (ss - sa); // when Anthony reaches safe zone
+			ld rt = (ri - rf) / ss;
+			if (t <= rt){
+				cout << t << endl;
+			}
+			else{
+				// this is good now!
+				ld ap = ai - sa*rt;
+				ld at = (ap - rf) / sa;
+				cout << rt + at << endl;
+			}
+		}
+	}
 
 	return 0;
 }
