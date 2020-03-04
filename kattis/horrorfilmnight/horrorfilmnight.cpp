882073576173b54ec60d30c406c3afbe145eaf53
gandhi56 <gandhi21299@gmail.com>
2020-03-03 22:44:41 -0700

    solved horror film night

--- kattis/horrorfilmnight/horrorfilmnight.cpp
+++ kattis/horrorfilmnight/horrorfilmnight.cpp
@@ -1,6 +1,7 @@
 #define INF 0x3f3f3f3f
 #define pb(x) push_back(x)
 #define sz(x) (int)(x).size()
+#define LIN cout << __LINE__ << endl;
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -11,12 +12,13 @@ typedef vector<bool> vb;
 typedef vector<vi> vii;
 typedef vector<vector<ii>> viii;
 
-const int sz = 10;
+const int sz = 1000001;
 
 int main(){
   ios_base::sync_with_stdio(0); cin.tie(0);
    
   vector<char> pref(sz, ' ');
+  
   int k;
   cin >> k;
   while (k--){
@@ -31,22 +33,28 @@ int main(){
     cin >> d;
     pref[d] = (pref[d] == 'X'? 'Z':'Y');
   }
-
-  int i = 0;
-  int ans = 1;
-  while (i+1 < sz){
-    // cout << i << endl;
-    if (pref[i] == 'X' or pref[i] == 'Y'){
-      if (pref[i+1] == pref[i]) break;  // can't watch the next movie
-      if (pref[i+1] != ' ') ans++;
-    }
-    else if (pref[i] == 'Z'){
+  
+  int ans = 0;
+  bool x = true;
+  bool y = true;
+  for (int i = 0; i < sz(pref); ++i){
+    if (pref[i] == 'Z'){
+      x = y = true;
       ans++;
     }
-    i++;
+    else if (pref[i] == 'X' and y){
+      x = true;
+      y = false;
+      ans++;
+    }
+    else if (pref[i] == 'Y' and x){
+      x = false;
+      y = true;
+      ans++;
+    }
   }
   cout << ans << endl;
-
+  
   return 0;
 }
 
