3a084a96bc6da3e5700357a84631044bab7eb018
Anshil Gandhi <Anshil.Gandhi@amd.com>
2022-12-28 23:12:46 -0700

    interactive ttt

--- kattis/interactivetictactoe/interactivetictactoe.cpp
+++ kattis/interactivetictactoe/interactivetictactoe.cpp
@@ -18,11 +18,10 @@ void read_state(State& s) {
     while (i < 9) {
         char c;
         cin >> c;
-        if (c == '\n')	cin >> c;
+        if (c == '\n')  cin >> c;
         s[i++] = c;
     }
 }
-
 
 pair<int, bool> eval(State s) {
     // check for endgame
@@ -30,26 +29,15 @@ pair<int, bool> eval(State s) {
     M['x'] = 1;
     M['o'] = -1;
 
-    // horizontal
-    if (s[0] == s[1] && s[1] == s[2] && s[0] != '.')
-        return {M[s[0]], true};
-    if (s[3] == s[4] && s[4] == s[5] && s[3] != '.')
-        return {M[s[3]], true};
-    if (s[6] == s[7] && s[7] == s[8] && s[6] != '.')
-        return {M[s[6]], true};
-    
-    // vertical
-    if (s[0] == s[3] && s[3] == s[6] && s[0] != '.')
-        return {M[s[0]], true};
-    if (s[1] == s[4] && s[4] == s[7] && s[1] != '.')
-        return {M[s[1]], true};
-    if (s[2] == s[5] && s[5] == s[8] && s[2] != '.')
-        return {M[s[2]], true};
+    auto match = [&](int i, int j, int k) {
+        return s[i] == s[j] && s[j] == s[k] && s[i] != '.';
+    };
 
-    // diagonal
-    if (s[0] == s[4] && s[4] == s[7] && s[0] != '.')
-        return {M[s[4]], true};
-    if (s[2] == s[4] && s[4] == s[6] && s[2] != '.')
+    if (match(0,1,2) || match(0,3,6))
+        return {M[s[0]], true};
+    if (match(2,5,8) || match(6,7,8))
+        return {M[s[2]], true};
+    if (match(0,4,8) || match(2,4,6) || match(3,4,5) || match(1,4,7))
         return {M[s[4]], true};
 
     // is there a move to play?
@@ -60,34 +48,29 @@ pair<int, bool> eval(State s) {
     return {0, true};   // draw
 }
 
+// returns {best value, best move}
 pair<int, int> best_move(State s, char p) {
-    // evaluate state, return value if done
-    auto score = eval(s);
-    if (score.second)
-        return {score.first, 123};
+    auto [score, done] = eval(s);
+    if (done)   return {score, 123};
 
-    int bv = (p == 'x') ? -100 : 100;   // init best value
-    int bm;
+    int bv, bm;
+    bv = (p == 'x' ? -100 : 100);
     for (int m = 0; m < 9; ++m) {
-        if (s[m] != '.') continue;
-
-        s[m] = p; // play move
+        if (s[m] != '.')    continue;
+        s[m] = p;   // play move
         auto [v, _] = best_move(s, (p == 'x' ? 'o':'x'));
+        s[m] = '.'; // undo move
 
         if ((p == 'x' && v > bv) || (p == 'o' && v < bv)) {
             bv = v;
             bm = m;
         }
-
-        s[m] = '.';   // clear last move
     }
     return {bv, bm};
 }
 
 int main() {
     State s;
-    s.assign(9, '.');
-
     string in;
     cin >> in;
 
